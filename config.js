3b788558880d0d2d31f52e29a17249019144c6d3
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-14 20:05:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var config = {
     title: 'Karadeniz Dolgusu, Kıyı ve Deniz Tahribi/ Black Sea Landfil, Coastal and Marine Degradation ',
     subtitle: '',
     byline: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'BAP_Mapping Metabolism of Istanbul_Interactive Mapping / MEF AAP_027',
     chapters: [
         {
             id: '1',
